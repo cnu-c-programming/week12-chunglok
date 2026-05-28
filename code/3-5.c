4d11dd11209bbce716da9ac98e338b35aa617503
최준화 <choijunhwa072@gmail.com>
2026-05-28 14:47:36 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -7,10 +7,17 @@ int main(int argc, const char* argv[]) {
     if (fp == NULL) {
         return 0;
     }
-
-
+    
     int sum = 0;
-
+    int num;
+    char err[64];
+    while(!feof(fp)){
+        fscanf(fp, "%d", &num);
+        fscanf(fp, "%s", &err);
+        sum += num;
+        if(err[0] < '0' || err[0] > '9')
+            fprintf(stderr, "invalid input %s\n", err);
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
