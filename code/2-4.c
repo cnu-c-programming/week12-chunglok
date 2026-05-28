bdadf080fa61686bc7f6adf7bdb5a1a4280a0851
최준화 <choijunhwa072@gmail.com>
2026-05-28 14:39:30 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,7 +6,18 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
-
+    int alphabet;
+    int count[26] = {0};
+    while(!feof(fp)){
+        fscanf(fp, "%c", &alphabet);
+        for(int i = 'a'; i <= 'z'; i++){
+            for(int j = 0; j < 26; j++){
+                if(alphabet == i)
+                    count[j]++;
+                    fprintf("%c: %d\n", alphabet, count[j]);
+            }
+        }
+    }
 
     fclose(fp);
 }
