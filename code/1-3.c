03a7f9f790df61c433fd7b400a5607cb5b1872b0
최준화 <choijunhwa072@gmail.com>
2026-05-28 23:22:42 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[], const char* envp[]) {
-    const char *e = envp[0];
+    const char *e = envp;
 
     while(*e != NULL) {
         printf("%s\n", e);
