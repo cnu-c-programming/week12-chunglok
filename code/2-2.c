90d8ab9b1ace4d6e4c27f3f43c8fb95245fa4b06
최준화 <choijunhwa072@gmail.com>
2026-05-28 14:38:42 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -11,12 +11,19 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-
+    while(!feof(fp))
+        for(int i = 0; i < sizeof(students)/sizeof(Student); i++){
+            fscanf(fp, "%s %d", students[i].name, &students[i].score);
+            count++;
+        }
 
     int max = 0;
     float avg = 0;
 
-
+    for(int i = 0; i < count; i++){
+        max = max + students[i].score;
+    }
+    avg = (float)max / count;
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
