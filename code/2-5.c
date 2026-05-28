f5978f257d31a1570a8324b2c2524874b3e7bc6a
최준화 <choijunhwa072@gmail.com>
2026-05-28 14:40:15 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -3,7 +3,14 @@
 int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    fscanf(fp_src, "%s", argv[1]);
+    fprintf(fp_dst, "%s", argv[1]);
+    
+    fclose(fp_src);
+    fclose(fp_dst);
 
 }
 
